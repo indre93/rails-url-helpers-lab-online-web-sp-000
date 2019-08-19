51cde2be109a47cd1365829d93ce07e890468543
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-19 23:04:41 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,8 +18,11 @@ class StudentsController < ApplicationController
   def activate
     @student = Student.find(params[:id])
 
-    @student.active = false if @student.active == true
-    @student.active if @student.active == false
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
 
     @student.save
     redirect_to student_path(@student)
