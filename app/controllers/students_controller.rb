95d6926d2ec12bd2250955b301f6e1c6b7288c94
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-19 21:58:48 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,13 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+
+    if @student.active == false 
+      @status = "This student is currently inactive."
+    else @student.active == true
+      @status = "This student is currently active."
+    end
+      
   end
 
   def activate
